61ad9d2b8c7d402456ea3b433b9681d927b43a6f
Steve Clair <sclair@bird-technologies.com>
2021-04-22 11:12:36 -0400

    logic added, styles updated

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //Selectors
 document.querySelector("form").addEventListener('submit', handleSubmitForm );
-document.querySelector('ui').addEventListener('click', handleCompletedItem)
+document.querySelector('ul').addEventListener('click', handleClickDeleteOrCheck);
+document.getElementById('clearAll').addEventListener(click, 'handleClearAll');
 
 //Event Handlers
 function handleSubmitForm(e) {
@@ -12,8 +13,19 @@ function handleSubmitForm(e) {
     }
 }
 
-function handleCompletedItem(){
-    
+function handleClickDeleteOrCheck(e){
+    if(e.target.name == 'checkButton' ){
+        checkTodo(e);
+    }
+
+    if(e.target.name == 'deleteButton'){
+        deleteTodo(e);
+    } 
+
+}
+
+function handleClearAll(){
+    document.querySelector(ui).innerHTML = ''
 }
 
 //Helpers
@@ -22,8 +34,30 @@ function addTodo (todo){
     let li = document.createElement('li');
 
     li.innerHTML = `
-    <span>${todo}</span>
+    <span class="todo-item">${todo}</span>
+    <button name="checkButton"><i class="fas fa-check-square"></i></button>
+    <button name="deleteButton" ><i class="fas fa-trash"></i></button>
     `
-    li.classList.add('todoListItem');
+    li.classList.add('todo-list-item');
     ul.appendChild(li)
+}
+
+function checkTodo(e){
+    let item = e.target.parentNode;
+
+    if(item.style.textDecoration == 'line-through'){
+        item.style.textDecoration = 'none';
+    }else{
+        item.style.textDecoration = 'line-through';
+    }
+}
+
+function deleteTodo(e){
+    let item = e.target.parentNode;
+
+        item.addEventListener('transitionend', function(){
+            item.remove();
+        })
+    
+    item.classList.add('todo-item-list-fall')
 }
